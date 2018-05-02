3fd4eed88da15ba38f343951155c3ab6e480f6c3
Marcello <marcel.buehler@uzh.ch>
2018-05-02 14:51:18 +0200

    implemented simplenet

--- src/models/simplenet.py
+++ src/models/simplenet.py
@@ -9,13 +9,13 @@ import util.gaze
 data_format = "channels_last"  # Change this to "channels_first" to run on GPU
 
 
-class VGGNet(BaseModel):
+class SimpleNet(BaseModel):
     """An example neural network architecture."""
 
-    def get_conv2d_multi(self, base_name, n, x, filters, kernel_size, strides, padding='same', data_format='channels_last'):
+    def get_conv2d_multi(self, base_name, n, x, filters, kernel_size, strides, padding='same', data_format='channels_last', activation=tf.nn.relu):
         for i in range(n):
             layer_name = "{}.{}-{}".format(base_name, i, filters)
-            x = self.get_conv2d(name=layer_name, x=x, filters=filters, kernel_size=kernel_size, strides=strides, padding=padding, data_format=data_format)
+            x = self.get_conv2d(name=layer_name, x=x, filters=filters, kernel_size=kernel_size, strides=strides, padding=padding, data_format=data_format, activation=activation)
         return x
 
     def get_conv2d(self, name, x, filters, kernel_size, strides, padding='same', data_format='channels_last', activation=tf.nn.relu):
@@ -47,41 +47,25 @@ class VGGNet(BaseModel):
         # This network has two scopes, 'conv' and 'fc'. Though in practise it makes little sense to
         # train the two parts separately, this is possible.
         with tf.variable_scope('conv'):
-            with tf.variable_scope('conv1-64'):
-                x = tf.layers.conv2d(x, filters=64, kernel_size=3, strides=1,
-                                     padding='same', data_format=data_format)
+            with tf.variable_scope('conv1-20'):
+                x = tf.layers.conv2d(x, filters=20, kernel_size=5, strides=1,
+                                     padding='same', data_format=data_format, activation=tf.nn.relu)
                 self.summary.filters('filters', x)
                 self.summary.feature_maps('features', x, data_format=data_format)
 
-            x = self.get_conv2d_multi("conv1", 1, x, filters=64, kernel_size=3, strides=1, padding='same', data_format=data_format)
+            x = self.get_max_pooling2d('maxpool1', x, pool_size=2, strides=2, padding='same', data_format=data_format)
 
-            x = self.get_max_pooling2d("maxpool1", x, pool_size=2, strides=2, padding='same', data_format=data_format)
+            x = self.get_conv2d_multi("conv2", 1, x, filters=50, kernel_size=5, strides=1, padding='same', data_format=data_format)
 
-            x = self.get_conv2d_multi("conv2", 2, x, filters=128, kernel_size=3, strides=1, padding='same', data_format=data_format)
-
-            x = self.get_max_pooling2d("maxpool2", x, pool_size=2, strides=2, padding='same', data_format=data_format)
-
-            x = self.get_conv2d_multi("conv3", 4, x, filters=256, kernel_size=3, strides=1, padding='same', data_format=data_format)
-
-            x = self.get_max_pooling2d("maxpool3", x, pool_size=2, strides=2, padding='same', data_format=data_format)
-
-            x = self.get_conv2d_multi("conv4", 4, x, filters=512, kernel_size=3, strides=1, padding='same', data_format=data_format)
-
-            x = self.get_max_pooling2d("maxpool4", x, pool_size=2, strides=2, padding='same', data_format=data_format)
-
-            x = self.get_conv2d_multi("conv5", 4, x, filters=512, kernel_size=3, strides=1, padding='same', data_format=data_format)
-
-            x = self.get_max_pooling2d("maxpool5", x, pool_size=2, strides=2, padding='same', data_format=data_format)
+            x = self.get_max_pooling2d('maxpool2', x, pool_size=2, strides=2, padding='same', data_format=data_format)
 
         with tf.variable_scope('fc'):
             # Flatten the 50 feature maps down to one vector
             x = tf.contrib.layers.flatten(x)
 
             # FC layer
-            x = tf.layers.dense(x, units=512, name='fc4', activation=tf.nn.relu)
-            x = tf.layers.dense(x, units=256, name='fc5', activation=tf.nn.relu)
-            x = tf.layers.dense(x, units=128, name='fc6', activation=tf.nn.relu)
-            x = tf.layers.dense(x, units=64, name='fc7', activation=tf.nn.relu)
+            x = tf.layers.dense(x, units=500, name='fc4', activation=tf.nn.relu)
+
             self.summary.histogram('fc7/activations', x)
 
             # Directly regress two polar angles for gaze direction
